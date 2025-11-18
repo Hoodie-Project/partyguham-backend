18c1ebcc0eee598880341022e9a111d538b26c02
InYong Jung <83904581+iysh321@users.noreply.github.com>
2025-11-18 18:24:32 +0900

    feat: position

--- src/main/java/com/partyguham/catalog/controller/PositionController.java
+++ src/main/java/com/partyguham/catalog/controller/PositionController.java
@@ -1,4 +1,23 @@
 package com.partyguham.catalog.controller;
 
+import com.partyguham.catalog.dto.response.PositionResponse;
+import com.partyguham.catalog.service.PositionService;
+import com.partyguham.common.annotation.ApiV2Controller;
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@ApiV2Controller
+@RequiredArgsConstructor
+@RequestMapping("/positions")
 public class PositionController {
+
+    private final PositionService positionService;
+
+    // GET /api/v2/positions?main=백엔드
+    @GetMapping
+    public List<PositionResponse> getByMain(@RequestParam String main) {
+        return positionService.getByMain(main);
+    }
 }
