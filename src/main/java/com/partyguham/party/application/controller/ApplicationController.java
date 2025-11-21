ceae0f599e27410752a69849692e6a09ed7143e3
younggalee <rkduddl1717@gmail.com>
2025-11-22 06:55:17 +0900

    feat: PartyService 파티생성 추가

--- src/main/java/com/partyguham/party/application/controller/ApplicationController.java
+++ src/main/java/com/partyguham/party/application/controller/ApplicationController.java
@@ -1,30 +1,30 @@
-package com.partyguham.party.application.controller;
-
-
-import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.*;
-
-@RestController
-@RequestMapping("/api/parties/{partyId}/applications")
-public class ApplicationController {
-    @PostMapping("/{partyApplicationId}/approval")
-    public ResponseEntity<MessageResponseDto> approvePartyApplication(
-            @PathVariable Long partyId,
-            @PathVariable Long partyApplicationId,
-            @RequestHeader("Authorization") String authorization
-    ) {}
-
-    @PostMapping("/{partyApplicationId}/rejection")
-    public ResponseEntity<MessageResponseDto> rejectPartyApplication(
-            @PathVariable Long partyId,
-            @PathVariable Long partyApplicationId,
-            @RequestHeader("Authorization") String authorization
-    ) {}
-
-    @DeleteMapping("/{partyApplicationId}")
-    public ResponseEntity<Void> deletePartyApplication(
-            @PathVariable Long partyId,
-            @PathVariable Long partyApplicationId,
-            @RequestHeader("Authorization") String authorization
-    ) {}
-}
+//package com.partyguham.party.application.controller;
+//
+//
+//import org.springframework.http.ResponseEntity;
+//import org.springframework.web.bind.annotation.*;
+//
+//@RestController
+//@RequestMapping("/api/parties/{partyId}/applications")
+//public class ApplicationController {
+//    @PostMapping("/{partyApplicationId}/approval")
+//    public ResponseEntity<MessageResponseDto> approvePartyApplication(
+//            @PathVariable Long partyId,
+//            @PathVariable Long partyApplicationId,
+//            @RequestHeader("Authorization") String authorization
+//    ) {}
+//
+//    @PostMapping("/{partyApplicationId}/rejection")
+//    public ResponseEntity<MessageResponseDto> rejectPartyApplication(
+//            @PathVariable Long partyId,
+//            @PathVariable Long partyApplicationId,
+//            @RequestHeader("Authorization") String authorization
+//    ) {}
+//
+//    @DeleteMapping("/{partyApplicationId}")
+//    public ResponseEntity<Void> deletePartyApplication(
+//            @PathVariable Long partyId,
+//            @PathVariable Long partyApplicationId,
+//            @RequestHeader("Authorization") String authorization
+//    ) {}
+//}
--- src/main/java/com/partyguham/party/recruitment/controller/RecruitmentAdminController.java
+++ src/main/java/com/partyguham/party/recruitment/controller/RecruitmentAdminController.java
@@ -1,45 +1,45 @@
-package com.partyguham.party.recruitment.controller;
-
-
-import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.*;
-
-@RestController
-@RequestMapping("/api/parties/{partyId}/admin/recruitments")
-public class RecruitmentAdminController {
-    @PatchMapping("/{partyRecruitmentId}/completed")
-    public ResponseEntity<MessageResponseDto> completePartyRecruitment(
-            @PathVariable Long partyId,
-            @PathVariable Long partyRecruitmentId,
-            @RequestHeader("Authorization") String authorization
-    ) {}
-
-    @PostMapping("/batch-status") // 주소변경 확인요망
-    public ResponseEntity<Void> completePartyRecruitmentBatch(
-            @PathVariable Long partyId,
-            @RequestHeader("Authorization") String authorization,
-            @RequestBody PartyRecruitmentIdsBodyRequestDto request
-    ) {}
-
-    @PatchMapping("/{partyRecruitmentId}") // 주소변경 확인요망
-    public ResponseEntity<PartyRecruitmentsResponseDto> updatePartyRecruitment(
-            @PathVariable Long partyId,
-            @PathVariable Long partyRecruitmentId,
-            @RequestHeader("Authorization") String authorization,
-            @RequestBody CreatePartyRecruitmentRequestDto request
-    ) {}
-
-    @DeleteMapping("/{partyRecruitmentId}")
-    public ResponseEntity<Void> deletePartyRecruitment(
-            @PathVariable Long partyId,
-            @PathVariable Long partyRecruitmentId,
-            @RequestHeader("Authorization") String authorization
-    ) {}
-
-    @PostMapping("/batch-delete")
-    public ResponseEntity<Void> deletePartyRecruitmentBatch(
-            @PathVariable Long partyId,
-            @RequestHeader("Authorization") String authorization,
-            @RequestBody artyRecruitmentIdsBodyRequestDto request
-    ) {}
-}
+//package com.partyguham.party.recruitment.controller;
+//
+//
+//import org.springframework.http.ResponseEntity;
+//import org.springframework.web.bind.annotation.*;
+//
+//@RestController
+//@RequestMapping("/api/parties/{partyId}/admin/recruitments")
+//public class RecruitmentAdminController {
+//    @PatchMapping("/{partyRecruitmentId}/completed")
+//    public ResponseEntity<MessageResponseDto> completePartyRecruitment(
+//            @PathVariable Long partyId,
+//            @PathVariable Long partyRecruitmentId,
+//            @RequestHeader("Authorization") String authorization
+//    ) {}
+//
+//    @PostMapping("/batch-status") // 주소변경 확인요망
+//    public ResponseEntity<Void> completePartyRecruitmentBatch(
+//            @PathVariable Long partyId,
+//            @RequestHeader("Authorization") String authorization,
+//            @RequestBody PartyRecruitmentIdsBodyRequestDto request
+//    ) {}
+//
+//    @PatchMapping("/{partyRecruitmentId}") // 주소변경 확인요망
+//    public ResponseEntity<PartyRecruitmentsResponseDto> updatePartyRecruitment(
+//            @PathVariable Long partyId,
+//            @PathVariable Long partyRecruitmentId,
+//            @RequestHeader("Authorization") String authorization,
+//            @RequestBody CreatePartyRecruitmentRequestDto request
+//    ) {}
+//
+//    @DeleteMapping("/{partyRecruitmentId}")
+//    public ResponseEntity<Void> deletePartyRecruitment(
+//            @PathVariable Long partyId,
+//            @PathVariable Long partyRecruitmentId,
+//            @RequestHeader("Authorization") String authorization
+//    ) {}
+//
+//    @PostMapping("/batch-delete")
+//    public ResponseEntity<Void> deletePartyRecruitmentBatch(
+//            @PathVariable Long partyId,
+//            @RequestHeader("Authorization") String authorization,
+//            @RequestBody artyRecruitmentIdsBodyRequestDto request
+//    ) {}
+//}
