0d6db52c262970ca396d737c0f2442cad650eb25
InYong Jung <83904581+iysh321@users.noreply.github.com>
2025-11-22 04:36:04 +0900

    feat: report

---
+++ src/main/java/com/partyguham/report/repository/ReportRepository.java
@@ -0,0 +1,13 @@
+package com.partyguham.report.repository;
+
+import com.partyguham.report.entity.Report;
+import com.partyguham.report.entity.ReportTargetType;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.List;
+
+public interface ReportRepository extends JpaRepository<Report, Long> {
+
+    // 어떤 대상에 대한 모든 신고 조회
+    List<Report> findByTargetTypeAndTargetId(ReportTargetType targetType, Long targetId);
+}
