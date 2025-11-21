c7093f2ac7f1a97d1c3fa85424829723b49270ae
InYong Jung <83904581+iysh321@users.noreply.github.com>
2025-11-21 22:49:55 +0900

    update: api prefix config

--- src/main/java/com/partyguham/config/ApiPrefixConfig.java
+++ src/main/java/com/partyguham/config/ApiPrefixConfig.java
@@ -1,5 +1,6 @@
 package com.partyguham.config;
 
+import com.partyguham.common.annotation.ApiV2Controller;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.servlet.config.annotation.PathMatchConfigurer;
@@ -10,8 +11,8 @@ public class ApiPrefixConfig implements WebMvcConfigurer {
 
     @Override
     public void configurePathMatch(PathMatchConfigurer configurer) {
-        configurer.addPathPrefix("/api/v2", c ->
-                c.isAnnotationPresent(RestController.class)
+        configurer.addPathPrefix("/api/v2",
+                c -> c.isAnnotationPresent(ApiV2Controller.class)
         );
     }
 }
