7ce313a0c0188b5bcac5f7028fb8ce9796a46b9b
InYong Jung <83904581+iysh321@users.noreply.github.com>
2025-11-24 20:39:17 +0900

    update: env path

--- src/main/java/com/partyguham/infra/fcm/FirebaseProperties.java
+++ src/main/java/com/partyguham/infra/fcm/FirebaseProperties.java
@@ -1,9 +1,11 @@
 package com.partyguham.infra.fcm;
 
 import lombok.Getter;
+import lombok.Setter;
 import org.springframework.boot.context.properties.ConfigurationProperties;
 
 @Getter
+@Setter
 @ConfigurationProperties(prefix = "firebase.service-account")
 public class FirebaseProperties {
     private String path;
