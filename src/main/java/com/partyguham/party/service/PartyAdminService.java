1e9fe47f86b72db14206829435d4e3f695d3c8f9
InYong Jung <83904581+iysh321@users.noreply.github.com>
2025-11-29 08:41:25 +0900

    delete: interface PartyAdminServiceImpl

--- src/main/java/com/partyguham/party/service/PartyAdminService.java
+++ src/main/java/com/partyguham/party/service/PartyAdminService.java
@@ -2,24 +2,61 @@ package com.partyguham.party.service;
 
 import com.partyguham.party.dto.partyAdmin.request.*;
 import com.partyguham.party.dto.partyAdmin.response.*;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
 
-public interface PartyAdminService {
-    
-    UpdatePartyResponseDto updateParty(Long partyId, Long userId, UpdatePartyRequestDto request); // 파티 정보 수정
-    
-    UpdatePartyStatusResponseDto updatePartyStatus(Long partyId, Long userId, UpdatePartyStatusRequestDto request); // 파티 상태 수정
-    
-    void deletePartyImage(Long partyId, Long userId); // 파티 이미지 삭제
-    
-    void deleteParty(Long partyId, Long userId); // 파티 삭제
-    
-    PartyDelegationResponseDto delegateParty(Long partyId, Long userId, PartyDelegationRequestDto request); // 파티 위임
-    
-    GetAdminPartyUsersResponseDto getPartyUsers(Long partyId, GetAdminPartyUsersRequestDto request, Long userId); // 파티 사용자 목록 조회
-    
-    UpdatePartyUserResponseDto updatePartyUser(Long partyId, Long partyUserId, Long userId, UpdatePartyUserRequestDto request); // 파티 사용자 수정
-    
-    void deletePartyUser(Long partyId, Long partyUserId, Long userId); // 파티 사용자 삭제
-    
-    void deletePartyUserBatch(Long partyId, Long userId, DeletePartyUsersBodyRequestDto request); // 파티 사용자 일괄 삭제
+@Service
+@RequiredArgsConstructor
+public class PartyAdminService {
+
+    public UpdatePartyResponseDto updateParty(Long partyId, Long userId, UpdatePartyRequestDto request) {
+        return null;
+    }
+
+
+    public UpdatePartyStatusResponseDto updatePartyStatus(Long partyId,
+                                                          Long userId,
+                                                          UpdatePartyStatusRequestDto request) {
+        return null;
+    }
+
+
+    public void deletePartyImage(Long partyId, Long userId) {
+
+    }
+
+
+    public void deleteParty(Long partyId, Long userId) {
+
+    }
+
+
+    public PartyDelegationResponseDto delegateParty(Long partyId, Long userId, PartyDelegationRequestDto request) {
+        return null;
+    }
+
+
+    public GetAdminPartyUsersResponseDto getPartyUsers(Long partyId,
+                                                       GetAdminPartyUsersRequestDto request,
+                                                       Long userId) {
+        return null;
+    }
+
+
+    public UpdatePartyUserResponseDto updatePartyUser(Long partyId,
+                                                      Long partyUserId,
+                                                      Long userId,
+                                                      UpdatePartyUserRequestDto request) {
+        return null;
+    }
+
+
+    public void deletePartyUser(Long partyId, Long partyUserId, Long userId) {
+
+    }
+
+
+    public void deletePartyUserBatch(Long partyId, Long userId, DeletePartyUsersBodyRequestDto request) {
+
+    }
 }
