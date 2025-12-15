200f5ca5fe30df34eea730b6926ddc343e7e31d8
younggalee <rkduddl1717@gmail.com>
2025-12-15 23:45:46 +0900

    update: 파티 목록 요청 DTO validation 추가

--- src/main/java/com/partyguham/party/dto/party/request/GetPartiesRequestDto.java
+++ src/main/java/com/partyguham/party/dto/party/request/GetPartiesRequestDto.java
@@ -1,18 +1,30 @@
 package com.partyguham.party.dto.party.request;
 
 import com.partyguham.party.entity.PartyStatus;
+import jakarta.validation.constraints.Min;
+import jakarta.validation.constraints.Positive;
 import lombok.Getter;
 import lombok.Setter;
 import java.util.List;
+import org.springframework.data.domain.Sort.Direction;
 
+/**
+ * 파티 목록 조회 요청 DTO
+ */
 @Getter
 @Setter
 public class GetPartiesRequestDto { 
-    private Integer page;
-    private Integer size;
-    private String sort;
-    private String order;
-    private PartyStatus partyStatus; 
-    private List<String> partyType;
+    @Min(1)
+    private Integer page = 1;
+
+    @Min(1)
+    private Integer size = 5;
+
+    private String sort = "createdAt";
+
+    private Direction order = Direction.ASC;       
+
+    private PartyStatus partyStatus = PartyStatus.IN_PROGRESS; 
+    private List<@Positive Long> partyType;
     private String titleSearch;
 }
