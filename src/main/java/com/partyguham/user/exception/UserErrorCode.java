01403d3459e8dd2e2fe252325b44d6b2f665a784
InYong Jung <83904581+iysh321@users.noreply.github.com>
2026-01-10 11:46:59 +0900

    fix: user ErrorCode

--- src/main/java/com/partyguham/user/exception/UserErrorCode.java
+++ src/main/java/com/partyguham/user/exception/UserErrorCode.java
@@ -9,19 +9,19 @@ import lombok.Getter;
 public enum UserErrorCode implements ErrorCode {
 
     // Core/Common 관련
-    USER_NOT_FOUND(404, "U001", "사용자를 찾을 수 없습니다."),
-    ACCOUNT_LOCKED(403, "U002", "계정이 잠겨 있습니다."),
-    USER_ALREADY_WITHDRAWN(409, "U003", "이미 탈퇴 처리된 사용자입니다."),
-    USER_STATUS_INACTIVE(409, "U004", "활성화되지 않은 계정 상태입니다."),
-    USER_PERMANENTLY_DELETED(409, "U005", "복구 불가능한 계정입니다."),
-    USER_RESTORE_PERIOD_EXPIRED(409, "U006", "계정 복구 가능 기간이 지났습니다."),
+    USER_NOT_FOUND(404, "U_001", "사용자를 찾을 수 없습니다."),
+    ACCOUNT_LOCKED(403, "U_002", "계정이 잠겨 있습니다."),
+    USER_ALREADY_WITHDRAWN(409, "U_003", "이미 탈퇴 처리된 사용자입니다."),
+    USER_STATUS_INACTIVE(409, "U_004", "활성화되지 않은 계정 상태입니다."),
+    USER_PERMANENTLY_DELETED(409, "U_005", "복구 불가능한 계정입니다."),
+    USER_RESTORE_PERIOD_EXPIRED(409, "U_006", "계정 복구 가능 기간이 지났습니다."),
 
 
-    DUPLICATE_EMAIL(409, "U101", "이미 존재하는 이메일 입니다."),
-    DUPLICATE_NICKNAME(409, "U102", "이미 존재하는 이메일 입니다."),
+    DUPLICATE_EMAIL(409, "U_101", "이미 존재하는 이메일 입니다."),
+    DUPLICATE_NICKNAME(409, "U_102", "이미 존재하는 이메일 입니다."),
 
     // Profile 관련
-    PROFILE_NOT_FOUND(404, "U101", "프로필 정보를 찾을 수 없습니다."),
+    PROFILE_NOT_FOUND(404, "U_101", "프로필 정보를 찾을 수 없습니다."),
 
     ;
     private final int status;
